96fb40ee5f5bdd077529186b68094891de9ba5d4
kostpik <38260652+kostpik@users.noreply.github.com>
2021-01-08 15:40:59 +0300

    first commit

---
+++ js/newjs.js
@@ -0,0 +1,19 @@
+"use strict";
+console.log("how are you");
+let number = 10;
+const str = "раскладка";
+console.log(number);
+console.log(number + str);
+ 
+const obj = {
+    age: 10,
+    name: `dfds`,
+    isMarried: false
+};
+console.log(obj.name);
+
+// const dialog = confirm( `вы хотите этого? ${obj.name}`);
+let incr = 10,
+    decr = 10;
+console.log(incr++);
+console.log(incr);
